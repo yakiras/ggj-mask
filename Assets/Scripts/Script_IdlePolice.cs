8c35c4a0aae74803b1ef59f3494ff5b3ddd3f74d
Yakira Sun <zhenzhen201303@gmail.com>
2026-01-27 01:35:54 -0700

    Added building fade

---
+++ Assets/Scripts/Script_IdlePolice.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using UnityEngine;
+
+public class IdlePolice : MonoBehaviour
+{
+    public PlayerDisguise playerController;
+    public GameStateManager gameStateManager;
+
+    public Sprite[] idle;
+    public Sprite[] shoot;
+    public Sprite[] hearts;
+    public float frameRate = 1.0f; // seconds per frame
+
+    private SpriteRenderer sr;
+    private Sprite[] currentAnimation;
+    private int currentFrame;
+    private float timer;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        SetAnimation(idle); // default animation
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        CheckDisguise();
+    }
+
+    void Update()
+    {
+        // Update frame
+        if (currentAnimation == null || currentAnimation.Length == 0) return;
+
+        timer += Time.deltaTime;
+        if (timer >= frameRate)
+        {
+            timer = 0f;
+            currentFrame = (currentFrame + 1) % currentAnimation.Length;
+            sr.sprite = currentAnimation[currentFrame];
+        }
+    }
+
+    void CheckDisguise()
+    {
+        switch (playerController.currentDisguise)
+        {
+            case "thief":
+                // ENDING 2: prison
+                gameStateManager.DisplayEnding(2);
+                break;
+            case "girl":
+                SetAnimation(hearts);
+                break;
+            case "thug":
+                break;
+        }
+    }
+
+    void SetAnimation(Sprite[] newAnimation)
+    {
+        if (newAnimation == currentAnimation) return;
+        currentAnimation = newAnimation;
+        currentFrame = 0;
+        timer = 0f;
+        if (currentAnimation.Length > 0)
+            sr.sprite = currentAnimation[0];
+    }
+}
